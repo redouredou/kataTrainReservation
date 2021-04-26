3d6347a3e6f2ff5af1b383a61070cc66c6c4cf73
EL JARRAÏ <eljarrai.reda@gmail.com>
2021-04-26 15:57:15 +0200

    add ticket office and first unit test

--- src/main/java/org/redarolla/TicketOffice.java
+++ src/main/java/org/redarolla/TicketOffice.java
@@ -2,9 +2,28 @@ package org.redarolla;
 
 public class TicketOffice {
 
+    private TrainDataService trainDataService;
+    private BookingReferenceService bookingReferenceService;
+
+    public TicketOffice(TrainDataService trainDataService, BookingReferenceService bookingReferenceService) {
+
+        this.trainDataService = trainDataService;
+        this.bookingReferenceService = bookingReferenceService;
+
+    }
+
     public Reservation makeReservation(ReservationRequest request) {
-        //TODO: implement this code!
-        return null;
+        String expectedBookingReference = bookingReferenceService.get();
+        TrainDataResponse trainDataResponse = trainDataService.get("1");
+        return new Reservation(null, null , "1524d");
+    }
+
+    public TrainDataResponse getTrainData(String trainId){
+        return trainDataService.get(trainId);
+    }
+
+    public String getBookingReference(){
+        return bookingReferenceService.get();
     }
 
 }
--- src/test/java/org/redarolla/TicketOfficeTest.java
+++ src/test/java/org/redarolla/TicketOfficeTest.java
@@ -1,12 +1,51 @@
 package org.redarolla;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TicketOfficeTest {
 
+    @Mock
+    TrainDataService trainDataService;
+
+    @Mock
+    BookingReferenceService bookingReferenceService;
+
     @Test
-    public void reserveSeats() {
-        // TODO: Write this code!
+    void it_should_return_valid_Reservation_seats_when_makeReservation(){
+
+        //GIVEN
+        String expectedTrainId = "1";
+        int numberOfSeats = 2;
+
+        String expectedBookingReference = "1524d";
+        when(bookingReferenceService.get()).thenReturn(expectedBookingReference);
+
+        Seat seat1A = new Seat("A", 1, "");
+        Seat seat2A = new Seat("A", 2, "");
+        List<Seat> expectedSeats = Arrays.asList(seat1A, seat2A);
+        TrainDataResponse trainDataResponse = new TrainDataResponse(expectedSeats);
+        when(trainDataService.get(expectedTrainId)).thenReturn(trainDataResponse);
+
+        TicketOffice ticketOffice = new TicketOffice(trainDataService, bookingReferenceService);
+
+        ReservationRequest reservationRequest = new ReservationRequest(expectedTrainId, numberOfSeats);
+
+        Reservation expectedReservation = new Reservation(expectedTrainId, expectedSeats, expectedBookingReference);
+        //WHEN
+        Reservation actualReservation = ticketOffice.makeReservation(reservationRequest);
+
+        //THEN
+        Assertions.assertThat(actualReservation.getBookingId()).isEqualTo(expectedReservation.getBookingId());
 
     }
 }
